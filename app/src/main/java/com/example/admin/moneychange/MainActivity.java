ffb5058b730d259b9bffe3c51658bb0812ff5fab
Fryann <fryann14@hotmail.com>
2018-03-04 20:34:42 -0400

    Design getting

--- app/src/main/java/com/example/admin/moneychange/MainActivity.java
+++ app/src/main/java/com/example/admin/moneychange/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.admin.moneychange;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements  Operation  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+    @Override
+    public void getUs()
+    {
+
+
     }
 }
---
+++ app/src/main/java/com/example/admin/moneychange/Operation.java
@@ -0,0 +1,19 @@
+package com.example.admin.moneychange;
+
+/**
+ * Created by Admin on 3/4/2018.
+ */
+
+public interface Operation {
+    // DOP = Dominican Pesos
+    //EU = Euros
+    //USD =  United Stated Dollars
+
+
+
+    double   dop = 0.0 , eu =0.0 ,dls = 0.0;
+
+
+      void  getUs();
+
+}
